6f22c3fe1cf4bc0228231eb699d706bf18d13135
manuel.meloni@edu.unife.it <manuel.meloni@edu.unife.it>
2024-05-06 08:02:02 +0200

    export of devs environment, added to env folder.

--- OpTreeMal/treeGUI.py
+++ OpTreeMal/treeGUI.py
@@ -1,17 +1,24 @@
 import tkinter as tk
-import tkinter.ttk as ttk
+import tkinter.ttk as ttk 
 
-# creazione finestra interfaccia
+#creazione finestra interfaccia
 screen = tk.Tk()
 screen.geometry("500x500")
 screen.title("OpTreeMal")
 
-# implementazione vista gerarchica
+#implementazione vista gerarchica
 treetime = ttk.Treeview(screen)
 
-# creazione colonne
-treetime["columns"] = ("Column 2", "Column 3", "Column 4", "Column 5")
-# formattazione colonne
+#creazione colonne
+treetime['columns'] = ("Column 2", "Column 3", "Column 4","Column 5")
+#formattazione colonne
+treetime.column("#0", width=50, minwidth=25)
+treetime.column("comlumn 2", width=50, minwidth=25)
+treetime.column("comlumn 2", width=50, minwidth=25)
+treetime.column("comlumn 2", width=50, minwidth=25)
+
+#definizione titolo colonne
 
 
 screen.mainloop()  # OR mainloop()
+
