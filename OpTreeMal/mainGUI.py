d43196e2e9d6a99d0e0e73b65bbd54273c8403aa
manuel.meloni@edu.unife.it <manuel.meloni@edu.unife.it>
2024-05-11 08:48:03 +0200

    UIs welcome window and treeSetting connected

--- OpTreeMal/mainGUI.py
+++ OpTreeMal/mainGUI.py
@@ -2,17 +2,37 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtWidgets import QWidget
+import sys
 
-class MyGUI(QMainWindow):
+class treeSettingUI(QMainWindow):
     def __init__(self):
-        super(MyGUI, self).__init__()
-        uic.loadUi('../doc/UIdocs/UI/mainUI.ui', self) #../doc/UIdocs/UI/mainUI.ui
+        super(treeSettingUI, self).__init__()
+        uic.loadUi('../doc/UIdocs/UI/treeSettingUI.ui', self)
         self.show()
 
+class welcomeUI(QMainWindow):
+    def __init__(self):
+        super(welcomeUI, self).__init__()
+
+        # carico la UI da un file .ui
+        uic.loadUi('../doc/UIdocs/UI/mainUI.ui', self)
+        
+        # definisco i widget
+        self.newTreeButton = self.findChild(QPushButton, 'newTreeButton')
+        self.randomTreeButton = self.findChild(QPushButton, 'randomTreeButton')
+        # collego i widget ai metodi
+        self.newTreeButton.clicked.connect(self.gotoTreeSetting)
+
+        # mostro la finestra
+        self.show()
+
+    def gotoTreeSetting(self):
+            self.treeSetting = treeSettingUI()
+            self.treeSetting.show()
 
 def main():
     app = QApplication([])
-    window = MyGUI()
+    window = welcomeUI()
     app.exec_()
 
 
